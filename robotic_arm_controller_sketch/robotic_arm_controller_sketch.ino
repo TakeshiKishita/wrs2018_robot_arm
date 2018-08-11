03d9001f7a7fb73df3b6e1070b408bb27f34821c
takeshi <tkishita.appqualia@gmail.com>
2018-08-11 18:13:32 +0900

    feat: first commit

--- robotic_arm_controller_sketch/robotic_arm_controller_sketch.ino
+++ robotic_arm_controller_sketch/robotic_arm_controller_sketch.ino
@@ -1,4 +1,23 @@
-const int analog0 = 0;
+const int controller_axis0 = 0;
+const int controller_axis1 = 1;
+const int controller_axis2 = 2;
+const int controller_axis3 = 3;
+const int controller_axis4 = 4;
+const int controller_axis5 = 5;
+
+const int arm_axis0_up = 2;
+const int arm_axis0_down = 3;
+const int arm_axis1_up = 4;
+const int arm_axis1_down = 5;
+const int arm_axis2_up = 6;
+const int arm_axis2_down = 7;
+const int arm_axis3_up = 8;
+const int arm_axis3_down = 9;
+const int arm_axis4_up = 10;
+const int arm_axis4_down = 11;
+const int arm_axis5_up = 12;
+const int arm_axis5_down = 13;
+
 
 void setup() {
   Serial.begin( 9600 );
