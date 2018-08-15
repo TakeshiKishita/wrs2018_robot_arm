96886ef17bc7bdd01ef97b8e4d6b613b231c85fb
takeshi <tkishita.appqualia@gmail.com>
2018-08-16 00:31:23 +0900

    fix: コード追記

--- robotic_arm_controller_sketch/robotic_arm_controller_sketch.ino
+++ robotic_arm_controller_sketch/robotic_arm_controller_sketch.ino
@@ -1,31 +1,56 @@
-const int controller_axis0 = 0;
-const int controller_axis1 = 1;
-const int controller_axis2 = 2;
-const int controller_axis3 = 3;
-const int controller_axis4 = 4;
-const int controller_axis5 = 5;
+#
+# ロボットアーム側制御
+#
+#
 
-const int arm_axis0_up = 2;
+const int arm_axis0_up   = 2;
 const int arm_axis0_down = 3;
-const int arm_axis1_up = 4;
+const int arm_axis1_up   = 4;
 const int arm_axis1_down = 5;
-const int arm_axis2_up = 6;
+const int arm_axis2_up   = 6;
 const int arm_axis2_down = 7;
-const int arm_axis3_up = 8;
+const int arm_axis3_up   = 8;
 const int arm_axis3_down = 9;
-const int arm_axis4_up = 10;
+const int arm_axis4_up   = 10;
 const int arm_axis4_down = 11;
-const int arm_axis5_up = 12;
+const int arm_axis5_up   = 12;
 const int arm_axis5_down = 13;
 
-
 void setup() {
-  Serial.begin( 9600 );
+    Serial.begin( 9600 );
+}
+
+int change_angle(int arm_angle, int controller_angle) {
+
+    int result;
+
+    result = x * y;
+    return result;
 }
 
 void loop() {
-  int i = analogRead( PIN_ANALOG_INPUT );
-  float f = i * 5.0 / 1023.0;
-  Serial.println( f );
-  delay( 1000 ); 
+
+    # アームの角度を読み取る
+    int arm_axis0 = analogRead(0);
+    int arm_axis1 = analogRead(1);
+    int arm_axis2 = analogRead(2);
+    int arm_axis3 = analogRead(3);
+    int arm_axis4 = analogRead(4);
+    int arm_axis5 = analogRead(5);
+
+    # コントローラーの角度を受信する
+    if (Serial.available() > 0){
+        String controller_angle_str = Serial.readStringUntil(';');
+    }
+    A = atoi(strtok(controller_angle_str,","));
+    Serial.println(A);         //1つ目のトークン表示
+
+    while(A!=NULL){            //トークンがNULLになるまでループ
+        A = atoi(strtok(NULL,","));  //2回目以降は第一引数はNULL
+        if(A!=NULL){
+            Serial.println(A);
+        }
+    }
+
+    delay(1000);
 }
