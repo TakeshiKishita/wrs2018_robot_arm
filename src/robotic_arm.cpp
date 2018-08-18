0089ed1ad4f5fa220bbea9027bedd9cac734a13e
takeshi <tkishita.appqualia@gmail.com>
2018-08-19 03:20:37 +0900

    fix: いったんできた？

--- src/robotic_arm.cpp
+++ src/robotic_arm.cpp
@@ -5,21 +5,19 @@
 //
 //
 
-const int arm_axis0_up   = 2;
-const int arm_axis0_down = 3;
-const int arm_axis1_up   = 4;
-const int arm_axis1_down = 5;
-const int arm_axis2_up   = 6;
-const int arm_axis2_down = 7;
-const int arm_axis3_up   = 8;
-const int arm_axis3_down = 9;
-const int arm_axis4_up   = 10;
-const int arm_axis4_down = 11;
-const int arm_axis5_up   = 12;
-const int arm_axis5_down = 13;
+// アームの軸数
+const int axis_num = 6;
+// モータードライバへ出力するoutチャンネル
+const int arm_axis_up[axis_num] = {2,4,6,8,10,12};
+const int arm_axis_down[axis_num] = {3,5,7,9,11,13};
 
 void setup() {
     Serial.begin( 9600 );
+
+    for (size_t i = 0; i < axis_num; i++) {
+      pinMode(arm_axis_up[i],OUTPUT);
+      pinMode(arm_axis_down[i],OUTPUT);
+    }
 }
 
 int change_angle(int arm_angle, int controller_angle) {
@@ -31,27 +29,45 @@ int change_angle(int arm_angle, int controller_angle) {
 void loop() {
 
     // アームの角度を読み取る
-    int arm_axis0 = analogRead(0);
-    int arm_axis1 = analogRead(1);
-    int arm_axis2 = analogRead(2);
-    int arm_axis3 = analogRead(3);
-    int arm_axis4 = analogRead(4);
-    int arm_axis5 = analogRead(5);
+    int arm_axis[axis_num];
+    arm_axis[0] = analogRead(0);
+    arm_axis[1] = analogRead(1);
+    arm_axis[2] = analogRead(2);
+    arm_axis[3] = analogRead(3);
+    arm_axis[4] = analogRead(4);
+    arm_axis[5] = analogRead(5);
 
     // コントローラーの角度を受信する
+    String controller_angle_str;
     if (Serial.available() > 0){
-        String controller_angle_str = Serial.readStringUntil(';');
-    }
-    char A;
-    A = atoi(strtok(controller_angle_str,","));
-    Serial.println(A);         //1つ目のトークン表示
-
-    while(A!=NULL){            //トークンがNULLになるまでループ
-        A = atoi(strtok(NULL,","));  //2回目以降は第一引数はNULL
-        if(A!=NULL){
-            Serial.println(A);
-        }
+        controller_angle_str = Serial.readStringUntil(';');
     }
 
-    delay(1000);
+    // CSV形式のコントローラ角度を各関節に分解する
+    int ctrl_axis[6];
+    ctrl_axis[0] = atoi(strtok(controller_angle_str.c_str(),","));
+    Serial.println(ctrl_axis[0]);         //1つ目のトークン表示
+    ctrl_axis[1] = atoi(strtok(NULL,","));
+    ctrl_axis[2] = atoi(strtok(NULL,","));
+    ctrl_axis[3] = atoi(strtok(NULL,","));
+    ctrl_axis[4] = atoi(strtok(NULL,","));
+    ctrl_axis[5] = atoi(strtok(NULL,","));
+
+    for(int x : ctrl_axis){
+	     if (ctrl_axis[x] > arm_axis[x]) {
+         //アームの角度が小さかった場合
+         digitalWrite(arm_axis_down[x],LOW);
+         digitalWrite(arm_axis_up[x],HIGH);
+       }else if (ctrl_axis[x] < arm_axis[x]) {
+         //アームの角度が大きかった場合
+         digitalWrite(arm_axis_up[x],LOW);
+         digitalWrite(arm_axis_down[x],HIGH);
+       } else {
+         //角度が同じだった場合
+         digitalWrite(arm_axis_down[x],LOW);
+         digitalWrite(arm_axis_up[x],LOW);
+       }
+     }
+
+    delay(100);
 }
