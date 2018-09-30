5ad187e06c803129675425d2006edad371490ded
takeshi <xxtkcxx@gmail..com>
2018-10-01 02:25:31 +0900

    i2c受信の修正

--- src/main.cpp
+++ src/main.cpp
@@ -1,76 +1,96 @@
 #include <Arduino.h>
+#include <Wire.h>
 
 //
 // ロボットアーム側制御
 //
 //
 
+const int slave_address = 0x10;
 // アームの軸数
 const int axis_num = 6;
 // モータードライバへ出力するoutチャンネル
 const int arm_axis_up[axis_num] = {2,4,6,8,10,12};
 const int arm_axis_down[axis_num] = {3,5,7,9,11,13};
 // 処理遅延時間
-const int delay_time = 1000;
+const int delay_time = 500;
+// アーム格納時の初期角度
+int initial_angle[axis_num];
+
+int ctrl_axis[6];
+void ReceiveMassage(int n){
+  Serial.print("Received ");
+  Serial.println(n);
+  // TODO 角度の読み取り方法を変更
+  for (int i = 0; i < n; i++) {
+    char c = Wire.read();
+    Serial.print("Byte ");
+    Serial.println((int)c);
+    ctrl_axis[i] = (int)c;
+  }
+
+  // コントローラーの角度を受信する
+  Serial.println("[get_num]");
+  // アームの角度を読み取る
+  int arm_axis[axis_num];
+  arm_axis[0] = analogRead(0);
+  arm_axis[1] = analogRead(1);
+  arm_axis[2] = analogRead(2);
+  arm_axis[3] = analogRead(3);
+  arm_axis[4] = analogRead(4);
+  arm_axis[5] = analogRead(5);
+
+  for (size_t x = 0; x < axis_num; x++) {
+    // トークンの値を出力
+    Serial.print(x);
+    Serial.print(" : ctrl_axis "+String(ctrl_axis[x]));
+    Serial.println(" : arm_axis "+String(arm_axis[x]));
+
+    if (initial_angle[x] > arm_axis[x]) {
+      // アームの角度が初期位置よりも小さかった場合
+      Serial.println("initial > arm");
+
+      digitalWrite(arm_axis_down[x],LOW);
+      digitalWrite(arm_axis_up[x],HIGH);
+    }else if (ctrl_axis[x] > arm_axis[x]) {
+       //アームの角度が小さかった場合
+       Serial.println("ctrl > arm");
+       digitalWrite(arm_axis_down[x],LOW);
+       digitalWrite(arm_axis_up[x],HIGH);
+    }else if (ctrl_axis[x] < arm_axis[x]) {
+       //アームの角度が大きかった場合
+       Serial.println("ctrl < arm");
+       digitalWrite(arm_axis_up[x],LOW);
+       digitalWrite(arm_axis_down[x],HIGH);
+    } else {
+       //角度が同じだった場合
+       Serial.println("ctrl == arm");
+       digitalWrite(arm_axis_down[x],LOW);
+       digitalWrite(arm_axis_up[x],LOW);
+    }
+  }
+}
 
 void setup() {
     Serial.begin( 9600 );
+    Wire.begin(slave_address);
+
+    // アーム格納時の初期角度を設定
+    initial_angle[0] = analogRead(0);
+    initial_angle[1] = analogRead(1);
+    initial_angle[2] = analogRead(2);
+    initial_angle[3] = analogRead(3);
+    initial_angle[4] = analogRead(4);
+    initial_angle[5] = analogRead(5);
 
     for (size_t i = 0; i < axis_num; i++) {
       pinMode(arm_axis_up[i],OUTPUT);
       pinMode(arm_axis_down[i],OUTPUT);
     }
+    Wire.onReceive(ReceiveMassage);
 }
 
 void loop() {
-
-    // アームの角度を読み取る
-    int arm_axis[axis_num];
-    arm_axis[0] = analogRead(0);
-    arm_axis[1] = analogRead(1);
-    arm_axis[2] = analogRead(2);
-    arm_axis[3] = analogRead(3);
-    arm_axis[4] = analogRead(4);
-    arm_axis[5] = analogRead(5);
-
-    // シリアル通信がない場合は処理しない
-    if (Serial.available() == 0){
-      delay(delay_time);
-      return;
-    }
-    // コントローラーの角度を受信する
-    String controller_angle_str = Serial.readStringUntil("\r\n");
-    Serial.println("[get_num]"+controller_angle_str);
-
-    // CSV形式のコントローラ角度を各関節に分解する
-    int ctrl_axis[6];
-    ctrl_axis[0] = atoi(strtok(controller_angle_str.c_str(),","));
-    ctrl_axis[1] = atoi(strtok(NULL,","));
-    ctrl_axis[2] = atoi(strtok(NULL,","));
-    ctrl_axis[3] = atoi(strtok(NULL,","));
-    ctrl_axis[4] = atoi(strtok(NULL,","));
-    ctrl_axis[5] = atoi(strtok(NULL,","));
-
-    for (size_t x = 0; x < axis_num; x++) {
-      // トークンの値を出力
-      Serial.print(x);
-      Serial.print(" : ctrl_axis "+String(ctrl_axis[x]));
-      Serial.println(" : arm_axis "+String(arm_axis[x]));
-
-	    if (ctrl_axis[x] > arm_axis[x]) {
-         //アームの角度が小さかった場合
-         digitalWrite(arm_axis_down[x],LOW);
-         digitalWrite(arm_axis_up[x],HIGH);
-      }else if (ctrl_axis[x] < arm_axis[x]) {
-         //アームの角度が大きかった場合
-         digitalWrite(arm_axis_up[x],LOW);
-         digitalWrite(arm_axis_down[x],HIGH);
-      } else {
-         //角度が同じだった場合
-         digitalWrite(arm_axis_down[x],LOW);
-         digitalWrite(arm_axis_up[x],LOW);
-      }
-    }
-
-    delay(delay_time);
+  //I2Cで受信したときに呼び出す関数を登録する
+  delay(delay_time);
 }
